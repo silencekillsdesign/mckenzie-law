610a95091c5cd100c8a0346e35ce91c5c210f87e
CJ Williams <cj@silencekillsdesign.com>
2023-06-21 14:45:30 -0500

    update footer.vue
    fix email address and alignment

--- components/sections/footer.vue
+++ components/sections/footer.vue
@@ -1,23 +1,21 @@
 <template>
-  <v-footer fluid class="ma-0 pa-0 position-sticky">  
-    <v-row no-gutters align="center" class="px-12 py-5  bg-black w-100 justify-space-between">
+  <v-footer fluid class="ma-0 pa-0 position-sticky">
+    <v-row no-gutters align="center" class="px-3 py-5 bg-black w-100 justify-space-between">
       <v-col>
         <p class="skd-logo-font">
-          &copy; {{ new Date().getFullYear() }} — McKenzie Law. 
+          &copy; {{ new Date().getFullYear() }} — McKenzie Law.
         </p>
       </v-col>
       <v-spacer></v-spacer>
-      <v-col class="justify-right">
-        <v-row class="justify-right">
-        <v-btn          
-          class="mx-4"
-          target="_blank"
-          icon="mdi-email"
-          href="mailto:wes@mckenzielaw.com"
-          variant="plain"
-          >wes@mckenzielaw.com</v-btn>
-        </v-row>
-      </v-col>      
+      <v-col class="d-flex align-end flex-column">
+          <v-chip
+            class="ma-2"
+            label
+            href="mailto:wes@mckenzielawpllc.com">
+            <v-icon start icon="mdi-email"></v-icon>
+            wes@mckenzielawpllc.com
+          </v-chip>
+      </v-col>
     </v-row>
   </v-footer>
 </template>
