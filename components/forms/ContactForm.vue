7cdd326f8c23fdad45c09f5941be5e56e36642a9
CJ Williams <cj@silencekillsdesign.com>
2023-08-05 20:17:25 -0500

    remove validation

--- components/forms/ContactForm.vue
+++ components/forms/ContactForm.vue
@@ -1,17 +1,14 @@
 <template>
     <v-container>
-        <form name="contactPage" action="/thanks" method="POST" data-netlify="true">
-          <input type="hidden" name="contact-page" value="contactPage" />
+        <form name="contactPage" action="/thanks" method="POST" netlify>
             <v-container>
                 <v-row>
-                  <input type="hidden" name="form-name" value="contactPage" />
+                  <!-- <input type="hidden" name="form-name" value="contactPage" /> -->
                     <v-col
                         cols="12"
                         md="6">
                         <v-text-field
                             v-model="name"
-                            :rules="nameRules"
-                            :counter="30"
                             label="Your Name"
                             required></v-text-field>
                     </v-col>
@@ -21,7 +18,6 @@
                         md="6">
                         <v-text-field
                             v-model="email"
-                            :rules="emailRules"
                             label="E-mail"
                             required></v-text-field>
                     </v-col>
@@ -42,36 +38,3 @@
     </v-container>
 </template>
 
-<script>
-  export default {
-    data: () => ({
-      valid: false,
-      name: '',
-      nameRules: [
-        value => {
-          if (value) return true
-
-          return 'Name is requred.'
-        },
-        value => {
-          if (value?.length <= 40) return true
-
-          return 'Name must be less than 40 characters.'
-        },
-      ],
-      email: '',
-      emailRules: [
-        value => {
-          if (value) return true
-
-          return 'E-mail is requred.'
-        },
-        value => {
-          if (/.+@.+\..+/.test(value)) return true
-
-          return 'E-mail must be valid.'
-        },
-      ],
-    }),
-  }
-</script>
